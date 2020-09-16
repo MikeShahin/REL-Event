560fc598a7cabe598bbf14f57b39d29af0356e6b
mikeshahin <gobias101@gmail.com>
2020-09-15 17:21:44 -0700

    added readme

--- bin/run.rb
+++ bin/run.rb
@@ -13,21 +13,3 @@ require_relative "../lib/category.rb"
 require_relative "../lib/saved_events.rb"
 
 CLI.new.run
-
-# black
-# light_black
-# red
-# light_red
-# green
-# light_green
-# yellow
-# light_yellow
-# blue
-# light_blue
-# magenta
-# light_magenta
-# cyan
-# light_cyan
-# white
-# light_white
-# default
